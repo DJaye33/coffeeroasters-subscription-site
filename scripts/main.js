4c26927faa6673b723a0e37f3a72be5f711fb363
Danny JeanLouis <dlouis3386@gmail.com>
2021-03-22 06:01:44 -0400

    mobile: finished header section

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,30 @@
-const toggleMenu = () => {
-  const hamburger = document.querySelector(".hamburger");
-  const close = document.querySelector(".close");
-  const overlay = document.querySelector(".overlay");
+const hamburger = document.querySelector(".hamburger");
+const close = document.querySelector(".close");
+const overlay = document.querySelector(".overlay");
 
-  hamburger.addEventListener("click", (evt) => {
-    evt.target.classList.toggle("inactive");
-    overlay.classList.toggle("overlay-slide");
-  });
+const showOverlay = () => {
+  hamburger.classList.add("inactive");
+  close.classList.remove("inactive");
+  overlay.classList.add("show");
 };
 
-toggleMenu();
+const hideOverlay = () => {
+  hamburger.classList.remove("inactive");
+  close.classList.add("inactive");
+  overlay.classList.remove("show");
+};
+
+const menuChangeOnResize = () => {
+  if (window.innerWidth >= 768) {
+    close.classList.add("inactive");
+    hamburger.classList.add("inactive");
+  } else if (window.innerWidth < 768) {
+    hamburger.classList.remove("inactive");
+    close.classList.add("inactive");
+    overlay.classList.remove("show");
+  }
+};
+
+hamburger.addEventListener("click", showOverlay);
+close.addEventListener("click", hideOverlay);
+window.addEventListener("resize", menuChangeOnResize);
